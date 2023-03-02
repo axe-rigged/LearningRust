9316e2b241cbc69854d4e1afd8cf20b3bc335a72
axe-rigged <alex20101@student.hamk.fi>
2023-03-02 23:17:56 +0200

    Learned how to add function for structures and updating

--- structure_learning/src/main.rs
+++ structure_learning/src/main.rs
@@ -4,8 +4,17 @@ struct Enemy {
     hp: u8,
 }
 
+//Not need to reference number because its fast copy.
+//impl Enemy {
+//  fn attack(&self, mod: u8) -> u8 {
+//      self.attack * mod
+//  }
+//}
+
 //We can create tuple structure for no need to name fields.
+//#[derive(Debug)]
 //struct Position(i32, i32, i32);
+//dbg!()
 
 fn main() {
     let enemy1 = Enemy {
@@ -21,7 +30,12 @@ fn main() {
     };
 
     enemy2.name = String::from("Bat");
-    if enemy1.alive == true || enemy2.alive == true {
+    if enemy_living(&enemy1) == true ||  enemy_living(&enemy2) == true {
         println!("Enemys living: {0}, {1}.\nEnemys hp: {2}, {3}", enemy1.name, enemy2.name, enemy1.hp, enemy2.hp);
     }
+
+}
+
+fn enemy_living(enemies: &Enemy) -> bool {
+    enemies.alive
 }

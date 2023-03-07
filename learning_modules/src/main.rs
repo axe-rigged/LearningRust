6f1f5b1415936457bd926dce7a2354cf197e5cc9
axe-rigged <alex20101@student.hamk.fi>
2023-03-07 20:40:29 +0200

    Vector learning and reference/ownership learning again.

--- learning_modules/src/main.rs
+++ learning_modules/src/main.rs
@@ -13,4 +13,24 @@ fn main() {
     };
 
     println!("{} {}", enemy.name, enemy.hp);
+
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    let y = &v[2];
+    println!("Y should be 3, {y}");
+
+    //When we are getting value from list/vector that is out of bound and we dont know that we should use Vec.Get().
+    //This way we can check is there value and handel error.
+    let seco: Option<&i32> = v.get(1);
+    match seco {
+        Some(seco) => println!("There was value and its {seco}"),
+        None => println!("No value was found"),
+    }
+
+    let mut dog = String::from("Dog");
+    dog.push_str(" is animal");
+    println!("My string: {}", dog);
+    //format!("{string 1} {string2}") lets ass format string inside "" and parse value to variable
 }

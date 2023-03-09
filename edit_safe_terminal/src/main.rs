63a09e5fd69c57265c854b485d392056c3b36f45
axe-rigged <alex20101@student.hamk.fi>
2023-03-09 16:04:35 +0200

    We get access to file.

--- edit_safe_terminal/src/main.rs
+++ edit_safe_terminal/src/main.rs
@@ -1,5 +1,7 @@
-// use std::env::args;
+use std::env;
+use std::fs::File; //File::create(), File::open()
 use std::io;
+use std::io::Read; //Need for reading, sad it is not inside fs::File, but Im not smart to understand that why
 
 enum Choice {
     Add,
@@ -9,34 +11,56 @@ enum Choice {
 //x == filename
 //Need more error handeling like option/result or unwrap_or()
 fn main() {
-    println!("What you want do for the file? Use numbers");
-    println!("1)Add text\n2)Backup\n");
+    //First value is target/debug/file. Need to read more how to fix this or how to go around this right
+    let file: Vec<_> = env::args().collect();
 
-    let mut choice = String::new();
+    if file.len() > 0 {
+        let file: &str = &file[1];
+        let e_file = File::open(file).expect("No file found");
 
-    io::stdin()
-        .read_line(&mut choice)
-        .expect("Could not read line");
+        println!("What you want do for the file? Use numbers");
+        println!("1)Add text\n2)Backup\n");
 
-    let choice_int: u8 = match choice.trim().parse() {
-        Ok(num) => num,
-        Err(_) => 2,
-    };
+        let mut choice = String::new();
 
-    let optio: Choice = match choice_int {
-        1 => Choice::Add,
-        2 => Choice::Copy,
-        _ => Choice::Copy,
-    };
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Could not read line");
+        //No point to first change number and then Choice/enum. Bad thing from me. Hashmap possible
+        let choice_int: u8 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => 2,
+        };
 
-    match optio {
-        Choice::Add => add_to_file(),
-        Choice::Copy => backup_file(),
-    };
+        let optio: Choice = match choice_int {
+            1 => Choice::Add,
+            2 => Choice::Copy,
+            _ => Choice::Copy,
+        };
+
+        match optio {
+            Choice::Add => add_to_file(&e_file),
+            // Choice::Add => {
+            //     let mut txt = String::new();
+            //     e_file
+            //         .read_to_string(&mut txt)
+            //         .expect("error while writing");
+            //     println!("{}", txt);
+            // }
+            Choice::Copy => backup_file(),
+        };
+    } else {
+        println!("There was no .txt file given.");
+        for arg in file.into_iter() {
+            println!("{}", arg);
+        }
+    }
 }
 
-fn add_to_file() {
-    println!("Add");
+fn add_to_file(mut x: &File) {
+    let mut txt = String::new();
+    x.read_to_string(&mut txt).expect("Error while writing");
+    println!("{}", txt);
 }
 
 fn backup_file() {
